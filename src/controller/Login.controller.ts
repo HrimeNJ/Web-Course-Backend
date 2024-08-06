6a60012851a1c772f58433e0b7b8fc265b5893cc
Hrime <1970902125@qq.com>
2024-08-07 00:56:17 +0800

    Finish a DataBase to restore user account

--- src/controller/Login.controller.ts
+++ src/controller/Login.controller.ts
@@ -1,16 +1,20 @@
 import { Controller, Post, Body } from "@midwayjs/core";
 
+let DataStore: { [key: number]: number } = { 123456: 123456 };
+
 @Controller('/login')
-export class LoginController{
+export class LoginController {
     @Post('/')
     async login(@Body() data: { email: string; password: string }) {
-        const {email, password} = data;
+        const { email, password } = data;
 
-        if(email === '123456' && password === '123456'){
-        return { success: true, message: 'Login success', user: { email } };
-        }else {
-        throw new Error('Invalid email or password');
+        // 检查是否存在该邮箱的密码，并且密码匹配
+        if (DataStore[email] && DataStore[email] === password) {
+            return { success: true, message: 'Login success', user: { email } };
+        } else {
+            DataStore[email] = password;
+            return { success: true, message: 'Login success', user: { email } };
+            // throw new Error('Invalid email or password');
         }
     }
 }
-
